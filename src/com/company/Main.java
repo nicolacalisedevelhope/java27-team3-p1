51b140c4a6688dcaab9a71930417b4952c343d52
BuildTools <unconfigured@null.spigotmc.org>
2024-06-13 15:38:45 +0200

    aggiornamento main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,42 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        boolean a = true;
+        do{
+            System.out.println("1 somma, 2 sottrazione, 3 moltiplicazione, 4 divisione, 5 potenza, 6 controllo pari o dispari, 7 esci");
+            int scelta = scanner.nextInt();
 
+            switch (scelta){
+                case 1:
+                    //somma();
+                    break;
+                case 2:
+                    //sottrazione();
+                    break;
+                case 3:
+                    //moltiplicazione();
+                    break;
+                case 4:
+                    //divisione();
+                    break;
+                case 5:
+                    //potenza();
+                    break;
+                case 6:
+                    //pariDispari();
+                    break;
+                case 7:
+                    a=false;
+                    System.out.println("Hai spento la calcolatrice");
+                default:
+                    System.out.println("Comando non conosciuto");
+            }
+        }while(a);
     }
 
     //metodo somma
