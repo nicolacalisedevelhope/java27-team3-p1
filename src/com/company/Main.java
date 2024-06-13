1f9dc3e14f7ae6e14ccc0fb49be20d7e51cba50c
BuildTools <unconfigured@null.spigotmc.org>
2024-06-13 15:18:50 +0200

    creazione metodi

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,38 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
     }
+
+    //metodo somma
+    public void somma(){
+
+    }
+
+    //metodo sottrazione
+    public void sottrazione(){
+
+    }
+
+    //metodo moltiplicazione
+    public void moltiplicazione(){
+
+    }
+
+    //metodo divisione
+    public void divisione(){
+
+    }
+
+    //metodo potenza
+    public void potenza(){
+
+    }
+
+    //metodo controllo pari o dispari
+    public void pariDispari(){
+
+    }
+
+
 }
